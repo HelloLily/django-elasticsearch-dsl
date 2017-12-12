9bcae0bc76141a809a9db526fdfd9ef77104605a
Hans Adema <hans.adema@wearespindle.com>
2017-12-12 17:25:21 +0100

    Use custom fixtures for test_indices because it overwrites the general fixtures definition

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -1,11 +1,22 @@
 from unittest import TestCase
-from mock import patch
 
 from django.conf import settings
+from mock import patch
 
+from django_elasticsearch_dsl import DocType
 from django_elasticsearch_dsl.indices import Index
 from django_elasticsearch_dsl.registries import DocumentRegistry
 from tests import fixtures
+
+
+class DocA1(DocType):
+    class Meta:
+        model = fixtures.ModelA
+
+
+class DocA2(DocType):
+    class Meta:
+        model = fixtures.ModelA
 
 
 class IndexTestCase(fixtures.WithFixturesMixin, TestCase):
@@ -14,14 +25,14 @@ class IndexTestCase(fixtures.WithFixturesMixin, TestCase):
         registry = DocumentRegistry()
         with patch('django_elasticsearch_dsl.indices.registry', new=registry):
             index = Index('test')
-            index.doc_type(fixtures.DocA1)
+            index.doc_type(DocA1)
             docs = list(registry.get_documents())
             self.assertEqual(len(docs), 1)
-            self.assertIs(docs[0], fixtures.DocA1)
+            self.assertIs(docs[0], DocA1)
 
-            index.doc_type(fixtures.DocA2)
+            index.doc_type(DocA2)
             docs = registry.get_documents()
-            self.assertEqual(docs, {fixtures.DocA1, fixtures.DocA2})
+            self.assertEqual(docs, {DocA1, DocA2})
 
     def test__str__(self):
         index = Index('test')
